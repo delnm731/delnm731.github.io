e3866ddcf39499aff69cfb7cf0d1dedcaa919ae4
Mariana Del Nogal <marianadelnogal@Marianas-MacBook-Pro-2.local>
2015-03-18 13:45:07 -0400

    javascript

---
+++ javascript/intro.js
@@ -0,0 +1,18 @@
+var name = prompt("what is your name?");
+
+var xholder = document.getElementById("xholder");
+xholder.innerHTML = name;
+
+var birthday = prompt("when is your birthday?");
+
+var yholder = document.getElementById("yholder");
+yholder.innerHTML = birthday;
+
+var color = prompt("what is your favorite color?");
+zholder.innerHTML = color;
+
+
+
+document.getElementById("xholder").innerHTML = name;
+document.getElementById("yholder").innerHTML = birthday;
+document.getElementById("zholder").innerHTML = color;
