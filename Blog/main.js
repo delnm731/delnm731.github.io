519229dc596de765bd7bdcb14b6a81a844c42ec5
Mariana Del Nogal <marianadelnogal@Marianas-MBP-2.wireless.newschool.edu>
2015-05-07 16:49:53 -0400

    blog

--- Blog/main.js
+++ Blog/main.js
@@ -2,8 +2,10 @@ $(function(){
     $(".hamburger").click(function(){
         $(".navigation").toggleClass("expand");
         $(".hamburger").toggleClass("expand");
-        
-    
-});
-    
+        if ( $(".hamburger").hasClass("expand")) {
+            $(".hamburger > img").attr("src", "resources/hide.jpg");
+        } else {
+            $(".hamburger > img").attr("src", "resources/hamburger.jpg");
+        }
+    });
 });
