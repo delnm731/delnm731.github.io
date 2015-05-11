7ed70abbc1a00dcd8ea02484a30c5ef3f4b0c1b6
Mariana Del Nogal <marianadelnogal@Marianas-MBP-2.home>
2015-05-11 12:34:43 -0400

    blog

--- Blog/main.js
+++ Blog/main.js
@@ -8,4 +8,60 @@ $(function(){
             $(".hamburger > img").attr("src", "resources/hamburger.jpg");
         }
     });
+    
+    $(".request").hide();
+    $(".overlay").hide();
+    $(".error").hide();
+    
+    
+    function myFunction() {
+            
+        if ($("#firstname").val() == "") {
+            $(".error").show();  
+            $(".overlay").show();
+            $("#blur-region").addClass("blur-filter"); 
+            reutrn;
+        }
+        else if ($("#lastname").val() == "") {
+            $(".error").show();  
+            $(".overlay").show();
+            $("#blur-region").addClass("blur-filter");     
+            return;
+        }
+                 
+        else if ($("#emailaddress").val() == "") {
+            $(".error").show();  
+            $(".overlay").show();
+            $("#blur-region").addClass("blur-filter");     
+            return;
+        }
+           
+                 
+        $.ajax(
+                {
+                    url: "../data.json",
+                    cache:false,
+                    complete: function(data){
+                    $(".request").show();  
+                    $(".overlay").show();
+                    $("#blur-region").addClass("blur-filter");
+                    }
+                }
+      
+            );
+
+};
+
+    $("#submit-btn").click(myFunction);
+   function closeButton() {
+        $(".request").hide();
+        $(".error").hide();
+        $(".overlay").hide(); 
+        $("#blur-region").removeClass("blur-filter");
+   }
+    
+    $(".close-request").click(closeButton);
+    
+    
 });
+
